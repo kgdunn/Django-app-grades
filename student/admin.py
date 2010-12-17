c90c590a6b36c1414ee0c5aa2a8f2cc2025c1560
Kevin Dunn <kevin.dunn@connectmv.com>
2010-12-17 09:08:22 -0600

    Added grades for assignment 4(a); modified model admin for tokens; updated sign-in template; display-grades template

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from grades.student.models import Student, WorkUnit, Question, Grade, Category, GradeSummary, CategorySummary, WorkUnitSummary, Token
 
+class TokenAdmin(admin.ModelAdmin):
+    list_per_page = 2000
+    list_display = ('student', 'token_address', 'has_been_used', )
+
 admin.site.register(Student)
 admin.site.register(WorkUnit)
 admin.site.register(Question)
@@ -9,4 +13,4 @@ admin.site.register(Category)
 admin.site.register(GradeSummary)
 admin.site.register(CategorySummary)
 admin.site.register(WorkUnitSummary)
-admin.site.register(Token)
+admin.site.register(Token, TokenAdmin)
