926bb913e652132ad89f2fc67e3ecdfac4daeeb5
Kevin Dunn <kevin.dunn@connectmv.com>
2010-12-23 12:43:00 -0600

    Minor tweaks to admin page

--- student/admin.py
+++ student/admin.py
@@ -5,10 +5,14 @@ class TokenAdmin(admin.ModelAdmin):
     list_per_page = 2000
     list_display = ('student', 'token_address', 'has_been_used', )
 
+class GradeAdmin(admin.ModelAdmin):
+    list_per_page = 1000
+    list_display = ('grade', 'grade_char', 'question', 'student')
+
 admin.site.register(Student)
 admin.site.register(WorkUnit)
 admin.site.register(Question)
-admin.site.register(Grade)
+admin.site.register(Grade, GradeAdmin)
 admin.site.register(Category)
 admin.site.register(GradeSummary)
 admin.site.register(CategorySummary)
